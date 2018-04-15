1c06ed68def760f5aecda593749974b38f9e0f2a
JoshuaMiller <admin@joshuamiller.info>
2018-04-14 22:34:34 -0400

    embedded assembly view support

--- PiServerLite/ViewCollection.cs
+++ PiServerLite/ViewCollection.cs
@@ -2,6 +2,7 @@
 using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.IO;
+using System.Reflection;
 
 namespace PiServerLite
 {
@@ -62,13 +63,43 @@ namespace PiServerLite
             return this;
         }
 
-        public ViewCollection AddFromAssembly(string path, string viewName = null)
+        /// <summary>
+        /// Adds a single view to the collection using the specified path.
+        /// </summary>
+        /// <param name="assembly">The assembly where the views are located.</param>
+        /// <param name="path">The assembly path to the view.</param>
+        /// <param name="viewName">The unique key used to retrieve the view.</param>
+        public ViewCollection AddFromAssembly(Assembly assembly, string path, string viewName = null)
         {
-            viewList[viewName ?? path] = () => LoadFromAssembly(path);
+            viewList[viewName ?? path] = () => LoadFromAssembly(assembly, path);
             return this;
         }
 
-        private string LoadFromExternal(string filename)
+        /// <summary>
+        /// Adds a collection of views found under the specified root assembly path.
+        /// </summary>
+        /// <param name="assembly">The assembly where the views are located.</param>
+        /// <param name="rootPath">The root path of the views.</param>
+        public ViewCollection AddAllFromAssembly(Assembly assembly, string rootPath)
+        {
+            var resourceNameArray = assembly.GetManifestResourceNames();
+
+            foreach (var resourceName in resourceNameArray) {
+                if (!resourceName.StartsWith(rootPath, StringComparison.OrdinalIgnoreCase))
+                    continue;
+
+                var x = rootPath.Length;
+                if (resourceName.Length >= x+1 && resourceName[x] == '.') x++;
+
+                var localName = resourceName.Substring(x);
+
+                viewList[localName] = () => LoadFromAssembly(assembly, resourceName);
+            }
+
+            return this;
+        }
+
+        private static string LoadFromExternal(string filename)
         {
             if (Path.DirectorySeparatorChar != '\\')
                 filename = filename.Replace("\\", Path.DirectorySeparatorChar.ToString());
@@ -84,9 +115,16 @@ namespace PiServerLite
             }
         }
 
-        private string LoadFromAssembly(string path)
+        private static string LoadFromAssembly(Assembly assembly, string path)
         {
-            throw new NotImplementedException();
+            using (var stream = assembly.GetManifestResourceStream(path)) {
+                if (stream == null)
+                    throw new ApplicationException($"Assembly View '{path}' could not be found!");
+
+                using (var reader = new StreamReader(stream)) {
+                    return reader.ReadToEnd();
+                }
+            }
         }
     }
 }
