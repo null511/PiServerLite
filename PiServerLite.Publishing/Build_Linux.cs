0554f5bbf065a6f5fa1d63d3dcfa1beccf9db869
JoshuaMiller <admin@joshuamiller.info>
2018-05-30 22:05:50 -0400

    photon initial

---
+++ PiServerLite.Publishing/Build_Linux.cs
@@ -0,0 +1,31 @@
+﻿using Photon.Framework.Agent;
+using Photon.Framework.Tasks;
+using PiServerLite.Publishing.Internal;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PiServerLite.Publishing
+{
+    public class Build_Linux : IBuildTask
+    {
+        public IAgentBuildContext Context {get; set;}
+
+        
+        public async Task RunAsync(CancellationToken token)
+        {
+            await BuildSolution();
+        }
+
+        private async Task BuildSolution()
+        {
+            var msBuild = new MsBuild(Context) {
+                Exe = "msbuild",
+                Filename = "PiServerLite.sln",
+                Configuration = "Release",
+                Platform = "Any CPU",
+            };
+
+            await msBuild.BuildAsync();
+        }
+    }
+}
