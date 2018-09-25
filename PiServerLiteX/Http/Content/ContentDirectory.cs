398f9a63c4140107e56ae83fb6a95c7a9e2dfc61
JoshuaMiller <admin@joshuamiller.info>
2018-09-25 09:53:08 -0400

    publish restore fix; remove old project files

--- PiServerLiteX/Http/Content/ContentDirectory.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace PiServerLite.Http.Content
-{
-    public class ContentDirectory
-    {
-        public bool IsSecure {get; set;}
-        public string DirectoryPath {get; set;}
-        public string UrlPath {get; set;}
-    }
-}
